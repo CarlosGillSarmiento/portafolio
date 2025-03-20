47ab25ce678f0b958b08a703a51c936cb44624c9
Carlos Gill <carlos.gill@kashio.net>
2025-03-20 18:04:33 -0500

    feat(sidebar): add items of sidebar

--- src/app/portafolio/app.routing.ts
+++ src/app/portafolio/app.routing.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from '../shared/pages/home-page/home-page.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProjectsComponent } from './projects/projects.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 
@@ -10,8 +10,8 @@ export const routes: Routes = [
     component: HomePageComponent
   },
   {
-    path: 'profile',
-    component: ProfileComponent
+    path: 'projects',
+    component: ProjectsComponent
   },
   {
     path: 'about-me',
