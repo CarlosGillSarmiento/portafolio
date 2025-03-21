87357b8325a45c97eef0cc54ebf7749f67d837f8
Carlos Gill <carlos.gill@kashio.net>
2025-03-21 11:10:08 -0500

    feat(header): update responsive design

--- src/app/shared/pages/header/header.component.ts
+++ src/app/shared/pages/header/header.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { BASE_PATH } from '../../../app.routes';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  showMenu = false;
 
   constructor(private router: Router) {}
 
   navigate(path: string) {
     this.router.navigate([`/${BASE_PATH}/${path}`]);
+  }
+
+  openMenu() {
+    this.showMenu = !this.showMenu;
   }
 }
