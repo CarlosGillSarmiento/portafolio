4a44175af566a061fb389a30167b33dc12f2742b
Carlos Gill <carlos.gill@kashio.net>
2025-03-21 15:04:32 -0500

    feat(style): update styles of header component.

--- src/app/shared/pages/header/header.component.ts
+++ src/app/shared/pages/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BASE_PATH } from '../../../app.routes';
 import { CommonModule } from '@angular/common';
@@ -10,16 +10,32 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   showMenu = false;
 
   constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
 
   navigate(path: string) {
     this.router.navigate([`/${BASE_PATH}/${path}`]);
+    this.showMenu = false;
+    this.checkScreenSize();
   }
 
   openMenu() {
     this.showMenu = !this.showMenu;
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    if (typeof window !== 'undefined') {
+      this.showMenu = window.innerWidth >= 768;
+    }
   }
 }
